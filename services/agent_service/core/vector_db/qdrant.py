ec7267e192d602f0fe0518cf31f0eaf63df3ddb4
firrexguptaji <amang5989@gmail,com>
2026-04-04 23:38:31 +0530

    feat(rag): implement vector DB with Qdrant (store + search working)

---
+++ services/agent_service/core/vector_db/qdrant.py
@@ -0,0 +1,53 @@
+
+from qdrant_client import QdrantClient
+from qdrant_client.http.exceptions import UnexpectedResponse
+from qdrant_client.models import VectorParams, Distance, PointStruct
+import logging
+from shared.config.settings import settings
+
+logger = logging.getLogger(__name__)
+
+class VectorDB:
+    def __init__(self):
+        self.client = QdrantClient(
+            host=settings.QDRANT_HOST,
+            port=settings.QDRANT_PORT
+        )
+        self.collection_name = settings.QDRANT_COLLECTION
+
+        self._create_collection()
+
+    def _create_collection(self):
+        try:
+            self.client.get_collection(self.collection_name)
+        except UnexpectedResponse:
+            self.client.create_collection(
+                collection_name=self.collection_name,
+                vectors_config=VectorParams(
+                    size=settings.VECTOR_SIZE,
+                    distance=Distance.COSINE
+                )
+            )
+        
+            
+
+    def insert(self, id: int, vector: list, payload: dict):
+        self.client.upsert(
+            collection_name=self.collection_name,
+            points=[
+                PointStruct(
+                    id=id,
+                    vector=vector,
+                    payload=payload
+                )
+            ]
+        )
+
+    def search(self, query_vector: list, limit: int = 3):
+        results = self.client.query_points(
+            collection_name=self.collection_name,
+            query=query_vector,
+            limit=limit
+        )
+
+        return results.points
--- shared/config/settings.py
+++ shared/config/settings.py
@@ -1,15 +1,44 @@
-# shared/config/settings.py
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
+    # -----------------------
+    # API Gateway
+    # -----------------------
     AGENT_SERVICE_URL: str = os.getenv(
         "AGENT_SERVICE_URL",
-        "http://localhost:8001/generate"  # fallback
+        "http://localhost:8001/generate"
     )
+
     REQUEST_TIMEOUT: int = int(os.getenv("REQUEST_TIMEOUT", 15))
+
+    # -----------------------
+    # LLM
+    # -----------------------
+    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
+
+    MODEL_NAME: str = os.getenv("MODEL_NAME", "gemini-2.5-flash")
+
+    # -----------------------
+    # Embedding
+    # -----------------------
+    EMBEDDING_MODEL: str = os.getenv(
+        "EMBEDDING_MODEL",
+        "models/gemini-embedding-001"
+    )
+
+    # -----------------------
+    # Vector DB (Qdrant)
+    # -----------------------
+    QDRANT_HOST: str = os.getenv("QDRANT_HOST", "localhost")
+    QDRANT_PORT: int = int(os.getenv("QDRANT_PORT", 6333))
+    QDRANT_COLLECTION: str = os.getenv("QDRANT_COLLECTION", "documents")
+
+    # IMPORTANT: update after checking embedding size
+    VECTOR_SIZE: int = int(os.getenv("VECTOR_SIZE", 3072))
+
 
 settings = Settings()
